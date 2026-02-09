af4541ffd12d51422e0bb3e114460c937372d75c
uyarbekir-creator <uyarbekir@gmail.com>
2026-02-08 22:18:13 -0800

    collectible

--- Assets/_GameAssets/Scripts/GamePlay/Player/PlayerInteractionController.cs
+++ Assets/_GameAssets/Scripts/GamePlay/Player/PlayerInteractionController.cs
@@ -4,14 +4,10 @@ public class PlayerInteractionController : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other) 
     {
-      if(other.CompareTag(Consts.WheatTypes.GOLD_WHEAT))
-      other.gameObject?.GetComponent<GoldWheatCollectible>().Collect();
-
-      if(other.CompareTag(Consts.WheatTypes.HOLY_WHEAT))
-      other.gameObject?.GetComponent<HolyWheatCollectible>().Collect();
-
-      if(other.CompareTag(Consts.WheatTypes.ROTTEN_WHEAT))
-      other.gameObject?.GetComponent<RottenWheatCollectible>().Collect();
+      if(other.gameObject.TryGetComponent<ICollectible>(out var collectible))
+      {
+        collectible.Collect();
+      }
     }
     
 }
