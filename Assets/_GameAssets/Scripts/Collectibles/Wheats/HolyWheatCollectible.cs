d58985d6763d7256602bcc636c000e36a571adac
uyarbekir-creator <uyarbekir@gmail.com>
2026-02-14 21:50:27 -0800

    boostf

--- Assets/_GameAssets/Scripts/Collectibles/Wheats/HolyWheatCollectible.cs
+++ Assets/_GameAssets/Scripts/Collectibles/Wheats/HolyWheatCollectible.cs
@@ -1,13 +1,32 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HolyWheatCollectible : MonoBehaviour, ICollectible
 {
     [SerializeField] private WheatDesignSO _wheatDesignSO;
     [SerializeField] private PlayerController _playerController;
+    [SerializeField] private PlayerStateUI _playerStateUI;
+
+
+    private RectTransform _playerBoosterTransform;
+    private Image _playerboosterImage;
+    
+    private void Awake()
+    {
+        _playerBoosterTransform = _playerStateUI.GetBoosterJumpTransform;
+        _playerboosterImage = _playerBoosterTransform.GetComponent<Image>();
+    }
 
     public void Collect()
     {
         _playerController.SetJumpForce(_wheatDesignSO.IncreaseDecreaseMultiplier, _wheatDesignSO.ResetBoostDuration);
+        
+        _playerStateUI.PlayBoosterUIAnimations(_playerBoosterTransform, _playerboosterImage,
+        _playerStateUI.GetHolyBoosterWheatImage, _wheatDesignSO.ActiveWheatSprite,
+        _wheatDesignSO.PassiveWheatSprite, _wheatDesignSO.ActiveWheatSprite,
+        _wheatDesignSO.PassiveWheatSprite, _wheatDesignSO.ResetBoostDuration);
+        
         Destroy(gameObject);
+
     }
 }
--- Assets/_GameAssets/Scripts/Collectibles/Wheats/RottenWheatCollectible.cs
+++ Assets/_GameAssets/Scripts/Collectibles/Wheats/RottenWheatCollectible.cs
@@ -1,13 +1,31 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RottenWheatCollectible : MonoBehaviour, ICollectible
 {
     [SerializeField] private WheatDesignSO _wheatDesignSO;
     [SerializeField] private PlayerController _playerController;
+    [SerializeField] private PlayerStateUI _playerStateUI;
+
+
+    private RectTransform _playerBoosterTransform;
+    private Image _playerboosterImage;
+    
+    private void Awake()
+    {
+        _playerBoosterTransform = _playerStateUI.GetBoosterSlowTransform;
+        _playerboosterImage = _playerBoosterTransform.GetComponent<Image>();
+    }
 
     public void Collect()
     {
         _playerController.SetMovementSpeed(_wheatDesignSO.IncreaseDecreaseMultiplier, _wheatDesignSO.ResetBoostDuration);
+        
+        _playerStateUI.PlayBoosterUIAnimations(_playerBoosterTransform, _playerboosterImage,
+        _playerStateUI.GetRottenBoosterWheatImage, _wheatDesignSO.ActiveWheatSprite,
+        _wheatDesignSO.PassiveWheatSprite, _wheatDesignSO.ActiveWheatSprite,
+        _wheatDesignSO.PassiveWheatSprite, _wheatDesignSO.ResetBoostDuration);
+        
         Destroy(gameObject);
     }
 }
