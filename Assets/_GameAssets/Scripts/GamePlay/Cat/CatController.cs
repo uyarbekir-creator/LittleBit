35b6845e1b427032445a7fceb01ab56905d262c1
uyarbekir-creator <uyarbekir@gmail.com>
2026-02-22 07:58:27 -0800

    intellisense fix

--- Assets/_GameAssets/Scripts/GamePlay/Cat/CatController.cs
+++ Assets/_GameAssets/Scripts/GamePlay/Cat/CatController.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CatController : MonoBehaviour
 {
+    [Header("Settings")]
+    [SerializeField] private float _defaultSpeed = 5f;
+    [SerializeField] private float _chaseSpeed = 7f;
+
     private NavMeshAgent _catAgent;
+
+    private bool _isWaiting;
 
     private void Awake()
     {
