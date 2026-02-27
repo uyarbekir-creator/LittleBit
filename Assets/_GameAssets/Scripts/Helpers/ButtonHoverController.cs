9640699057b2b3aabbb69a667854da14a38b0f71
uyarbekir-creator <uyarbekir@gmail.com>
2026-02-26 19:00:14 -0800

    hover

---
+++ Assets/_GameAssets/Scripts/Helpers/ButtonHoverController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonHoverController : MonoBehaviour, IPointerEnterHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        AudioManager.Instance.Play(SoundType.ButtonHoverSound);
+}
+
+}
